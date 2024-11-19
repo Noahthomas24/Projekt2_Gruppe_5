33a33dcbf30ff8f95ec0611c69ad4fc2364726a4
juliusbay <juax0001@stud.kea.dk>
2024-11-19 16:04:12 +0100

    First commit - class medlem og metoder til at bestemme alder, medlemsstatus, abonnementspris og restance.

---
+++ src/medlem.java
@@ -0,0 +1,73 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class medlem {
+    protected final String navn;
+    protected final int alder;
+    protected int saldo;
+    protected String betalingsstatus;
+
+    protected enum medlemsStatus {AKTIV_JUNIOR, AKTIV_SENIOR, AKTIV_PENSIO, PASSIV}
+    protected medlemsStatus status;
+    protected String aktivPassiv;
+
+    medlem(String navn, LocalDate dateOfBirth, String aktivPassiv, String betalingsStatus) {
+        this.navn = navn;
+        alder = beregnAlder(dateOfBirth).getYears();
+        this.aktivPassiv = aktivPassiv;
+        this.betalingsstatus = betalingsStatus;
+        bestemMedlemsStatus();
+        medlemsPriser();
+        betaltEllerKredit();
+    }
+
+    protected Period beregnAlder(LocalDate dateOfBirth) {
+        LocalDate dagsDato = LocalDate.now();
+        Period alder = Period.between(dateOfBirth, dagsDato);
+        return alder;
+    }
+
+    protected void bestemMedlemsStatus() {
+        if (aktivPassiv.contains("Passiv")) status = medlemsStatus.PASSIV;
+        if (alder < 18) status = medlemsStatus.AKTIV_JUNIOR;
+        if (alder > 18 && alder < 60) status = medlemsStatus.AKTIV_SENIOR;
+        if (alder > 60) status = medlemsStatus.AKTIV_PENSIO;
+    }
+
+    protected int medlemsPriser(){
+        if (status == medlemsStatus.AKTIV_JUNIOR) saldo = 1000;
+        if (status == medlemsStatus.AKTIV_SENIOR) saldo = 1600;
+        if (status == medlemsStatus.AKTIV_PENSIO) saldo = 1200;
+        if (status == medlemsStatus.PASSIV) saldo = 500;
+        return saldo;
+    }
+
+    protected void betaltEllerKredit(){
+        if (betalingsstatus.contains("BETALT")) saldo = 0;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public medlemsStatus getMedlemsStatus() {
+        return status;
+    }
+
+    public int getSaldo(){
+        return saldo;
+    }
+
+    public String toString(){
+        return navn + " " + alder +" år - " +status;
+    }
+
+    public static void main(String[] args) {
+        medlem a = new medlem("Julius", LocalDate.of(2000, 01, 29), "Aktiv", "BETALT");
+        System.out.println(a + ""+a.getSaldo());
+    }
+}
