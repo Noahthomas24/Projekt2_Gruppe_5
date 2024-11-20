91100a70bf2b3333e99b4aff73c8eb46490283bc
NoahThomas24 <noth0001@stud.kea.dkgit config --global user.email noth0001@stud.kea.dk>
2024-11-20 15:07:08 +0100

    Betalingsstatus og restancelise

--- src/medlem.java
+++ src/medlem.java
@@ -1,12 +1,14 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 
 public class medlem {
     protected final String navn;
     protected final int alder;
     protected int saldo;
     protected String betalingsStatus;
-
+    static ArrayList<medlem> restanceListe = new ArrayList<>();
     protected enum medlemsStatus {AKTIV_JUNIOR, AKTIV_SENIOR, AKTIV_PENSIO, PASSIV}
     protected medlemsStatus status;
     protected String aktivPassiv;
@@ -44,8 +46,16 @@ public class medlem {
 
     protected void betaltEllerKredit(){
         if (betalingsStatus.equalsIgnoreCase("Betalt")) saldo = 0;
+        if (betalingsStatus.equalsIgnoreCase("kredit")) restanceListe.add(this);
     }
-
+    public String getBetalingsStatus() {
+        if (betalingsStatus.equalsIgnoreCase("betalt")){
+            return navn+" har ingen udestående betalinger. Saldoen ligger på: "+saldo;
+        }else return navn + " mangler at betale: "+saldo;
+    }
+    public List getRestanceListe(){
+        return restanceListe;
+    }
     public String getNavn() {
         return navn;
     }
@@ -68,6 +78,11 @@ public class medlem {
 
     public static void main(String[] args) {
         medlem a = new medlem("Julius", LocalDate.of(2000, 01, 29), "Aktiv", "Kredit");
+        medlem a1 = new medlem("Gulius", LocalDate.of(2000, 01, 29), "Aktiv", "Kredit");
+        medlem a2 = new medlem("Pulius", LocalDate.of(2000, 01, 29), "Aktiv", "betalt");
+
         System.out.println(a + " - "+a.getSaldo()+" kr.");
+        System.out.println(restanceListe);
+
     }
 }
