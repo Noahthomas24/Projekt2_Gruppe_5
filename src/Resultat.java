098562f981b13b38b4076059c34e8b1dac193d93
safirestorm <sarahfagerstroem@gmail.com>
2024-12-02 13:55:21 +0100

    Opdateret Resultat class

--- src/Resultat.java
+++ src/Resultat.java
@@ -1,13 +1,13 @@
+import java.sql.Array;
 import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
-public class Resultat {
+public class Resultat implements Comparable<Resultat>{
     static Scanner keyboard = new Scanner(System.in);
-    static ArrayList<Resultat> resultater = new ArrayList<>();
+    static List<Resultat> resultater = new ArrayList<>();
     String navn;
     String disciplin;
     LocalTime resTid;
@@ -23,8 +23,8 @@ public class Resultat {
         return navn+": "+resTid.format(format);
     }
 
-    public int compareTo(Resultat r){
-        return resTid.compareTo(r.resTid);
+    public int compareTo(Resultat other){
+        return this.resTid.compareTo(other.resTid);
     }
 
     public void addResultat(){
@@ -46,7 +46,7 @@ public class Resultat {
         boolean korrektTid = false;
 
         while (!korrektTid){
-            System.out.println("Indtast tid (mm:ss)");
+            System.out.println("Indtast tid (hh:mm:ss)");
             String indtastetTid = keyboard.nextLine();
 
             try {
@@ -57,14 +57,6 @@ public class Resultat {
             }
         }
         return tid;
-    }
-
-    public Duration konverterTid(){
-        LocalTime startTid = LocalTime.of(0,0);
-        LocalTime slutTid = verificerTid();
-
-        Duration resultat = Duration.between(startTid, slutTid);
-        return resultat;
     }
 
     public static void main(String[] args) {
@@ -78,9 +70,10 @@ public class Resultat {
         resultater.add(new Resultat("Odaae", "Rygcrawl", LocalTime.of(0,40,12,100000000)));
         resultater.add(new Resultat("Odan", "Rygcrawl", LocalTime.of(0,29,9,100000000)));
 
-        r1.konverterTid();
-        System.out.println(r1.konverterTid());
-
+        resultater.sort(null);
+        for (Resultat r:resultater){
+           System.out.println(r);
+        }
 
     }
 }
