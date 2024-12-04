420ea912ef5ce2a12b1f89083aff02b4b5f43d21
safirestorm <sarahfagerstroem@gmail.com>
2024-12-04 11:18:30 +0100

    Tiljøjet indtastDato metode

--- src/TryCatch.java
+++ src/TryCatch.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class TryCatch {
 
     public static int indtastTal(){
@@ -10,4 +13,24 @@ public class TryCatch {
             }
         }
     }
+
+    public static LocalDate indtastDato(){
+        LocalDate dato = null;
+        boolean korrektDato = false;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        while (!korrektDato){
+            System.out.println("Indtast dato (yyyy-mm-dd)");
+            String indtastetDato = Medlem.scanner.nextLine();
+            try {
+                dato = LocalDate.parse(indtastetDato, formatter);
+                korrektDato = true;
+            } catch (RuntimeException e) {
+                System.out.println("Ugyldigt input, prøv igen.");
+            }
+        }
+        return dato;
+    }
+
 }
+
